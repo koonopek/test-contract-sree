2c0d2c8e67c4cf147df7da98820bbeb5df8d341c
Michał Konopka <konopek68@gmail.com>
2023-03-28 10:38:59 +0200

    change to different contract

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,14 @@ export default function Home() {
   const [contractId, setContractId] = useState()
 
   useEffect(() => {
-    const warp = WarpFactory.forMainnet().use(new DeployPlugin())
-    warpRef.current = warp 
+    const warp = WarpFactory.forMainnet({ dbLocation: '', inMemory: true }).use(new DeployPlugin());
+    warpRef.current = warp
 
     setContractId(localStorage.getItem('contract-id'))
   }, [])
 
   const deploy = async () => {
-    const sourceId = `w2Ae8elQ0qVy2Ya82qthKn3FHfwTnTlpxzycT1OOAoc`
+    const sourceId = 'YaC9_fH46hY9C5iFrNkEokTKl8ds6Rn5xzkzHq8xYk0' //`w2Ae8elQ0qVy2Ya82qthKn3FHfwTnTlpxzycT1OOAoc`
 
     window.ethereum.request({ method: 'eth_requestAccounts' })
 
@@ -29,38 +29,51 @@ export default function Home() {
 
     const publicKey = bundlr.getSigner().publicKey
     const ownerHash = await Arweave.crypto.hash(publicKey);
-    const owner = Arweave.utils.bufferTob64Url(ownerHash);
+    // const owner = Arweave.utils.bufferTob64Url(ownerHash);
+    const owner = await provider.getSigner().getAddress();
 
-      const contractUpload = await bundlr.upload('', {
-        tags: [
-            { name: 'Init-State', value: JSON.stringify({ ...initialState, owner }) },
-            { name: 'App-Name', value: 'SmartWeaveContract' },
-            { name: 'App-Version', value: '0.3.0' },
-            { name: 'Content-Type', value: 'application/json' },
-            { name: 'Contract-Src', value: sourceId }
-        ]
+    const contractUpload = await bundlr.upload('', {
+      tags: [
+        {
+          name: 'Init-State', value: JSON.stringify({
+            description: 'This is the test of Atomic Asset token',
+            symbol: 'atomic-asset-test',
+            name: 'Sample Atomic Asset token',
+            decimals: 2,
+            totalSupply: 100,
+            balances: {
+              [owner]: 100,
+            },
+            allowances: {},
+            owner: owner,
+          })
+        },
+        { name: 'App-Name', value: 'SmartWeaveContract' },
+        { name: 'App-Version', value: '0.3.0' },
+        { name: 'Content-Type', value: 'application/json' },
+        { name: 'Contract-Src', value: sourceId }
+      ]
     })
 
-      const contractDeploy = await warpRef.current.register(contractUpload.id, 'node2')
+    const contractDeploy = await warpRef.current.register(contractUpload.id, 'node2')
 
-      localStorage.setItem('contract-id', contractDeploy.contractTxId)
+    localStorage.setItem('contract-id', contractDeploy.contractTxId)
 
-      setContractId(contractDeploy.contractTxId)
+    setContractId(contractDeploy.contractTxId)
   }
 
   const test = async () => {
     const { evmSignature } = await import('warp-contracts-plugin-signature')
 
-    const contract = warpRef.current.contract(contractId)
-    contract.connect({ type: 'ethereum', signer: evmSignature })
+    const contract = warpRef.current.contract(contractId).connect({ type: 'ethereum', signer: evmSignature })
 
-    await contract.writeInteraction({ function: "insert", id: 'id1', name: 'file1', contentType: 'image/jpeg' })
+    await contract.writeInteraction({ function: "transfer", amount: 10, to: 'bcc' })
 
     const state = (await contract.readState()).cachedValue.state
- 
+
     console.log(state)
   }
-  
+
   return (
     <>
       {contractId ?? 'contract not deployed yet'}
